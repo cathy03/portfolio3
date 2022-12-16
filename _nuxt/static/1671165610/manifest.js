a43cf2f6681b80ae1da4e488efe27168b0d9626f
cathy03 <yoonyeojooncathy@gmail.com>
2022-12-16 13:41:31 +0900

    Add files via upload

---
+++ _nuxt/static/1671165610/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fkakaoi","\u002Fxdraco","\u002F"]})
---
+++ _nuxt/static/1671165610/xdraco/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fportfolio3\u002F_nuxt\u002Fstatic\u002F1671165610",layout:"default",error:b,state:{app:{scroll:{isScrolling:false,limit:{x:a,y:a},x:a,y:a}}},serverRendered:true,routePath:"\u002Fxdraco",config:{_app:{basePath:"\u002Fportfolio3\u002F",assetsPath:"\u002Fportfolio3\u002F_nuxt\u002F",cdnURL:b}}}}(0,null));
