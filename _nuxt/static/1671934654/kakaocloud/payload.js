516d0a716a3032b3284ce076b5a677b4fa4a7323
cathy03 <yoonyeojooncathy@gmail.com>
2023-01-01 22:03:39 +0900

    Delete _nuxt directory

--- _nuxt/static/1671934654/kakaocloud/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/kakaocloud", {data:[{}],fetch:{},mutations:[]});
--- _nuxt/static/1671934654/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fkakaowork","\u002Fkakaoi","\u002Fabout","\u002Fkakaocloud","\u002Fxdraco","\u002Fheykakao"]})
